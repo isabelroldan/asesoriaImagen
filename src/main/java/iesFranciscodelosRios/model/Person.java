a9f6e82aa5962a0f30e948cfe80feb3451191ded
Isabel <isabelroldancordoba@hotmail.com>
2023-04-19 13:45:20 +0200

    create project

---
+++ src/main/java/iesFranciscodelosRios/model/Person.java
@@ -0,0 +1,11 @@
+package iesFranciscodelosRios.model;
+
+import java.util.Date;
+
+public class Person {
+    protected String dni;
+    protected String name;
+    protected String lastname;
+    protected Date bithday;
+    protected String phone;
+}
