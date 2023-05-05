79e7176e8d568f8e5c5d8b8ad398c3162ae7af70
Isabel <isabelroldancordoba@hotmail.com>
2023-05-06 01:23:54 +0200

    appointmentDAO casi acabada

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@
 Lsrc/main/resources/iesfranciscodelosrios/dam/imageconsulting/hello-view.fxml,7\8\78df27f9d57b82d97b2d9f72b52cae4c34ae906f
 N
 src/main/java/module-info.java,1\c\1c89a5491fbfda3fb050196d6a239f20200296e7
+p
+@src/main/java/iesfranciscodelosrios/dam/model/domain/Client.java,7\a\7a53d2d3fde9f2077889e132f328fc3b6e642c98
--- src/main/java/iesfranciscodelosrios/dam/model/dao/AppointmentDAO.java
+++ src/main/java/iesfranciscodelosrios/dam/model/dao/AppointmentDAO.java
@@ -1,34 +1,161 @@
 package iesfranciscodelosrios.dam.model.dao;
 
+import iesfranciscodelosrios.dam.model.connections.Connect;
 import iesfranciscodelosrios.dam.model.domain.Appointment;
+import iesfranciscodelosrios.dam.model.domain.Client;
+import iesfranciscodelosrios.dam.model.domain.Space;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AppointmentDAO implements DAO<Appointment> {
-    @Override
-    public List<Appointment> findAll() throws SQLException {
-        return null;
+
+    private final static  String FINDALL = "SELECT * FROM appointment";
+    private final static  String FINDBYID = "SELECT * FROM appointment WHERE id_appointment = ?";
+    private final static  String INSERT = "INSERT INTO appointment(id_appointment, startTime, endTime, date, id_client, id_space) VALUES(?, ?, ?, ?, ?, ?)";
+    private final static  String UPDATE = "UPDATE appointment SET startTime = ?, endTime = ?, date = ?, id_client= ?, id_space = ?";
+    private final static  String DELETE = "DELETE FROM appointment WHERE id_appointment = ?";
+
+    private Connection conn;
+
+    public AppointmentDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public AppointmentDAO() {
+        this.conn = Connect.getConnect();
     }
 
     @Override
-    public Appointment findById(int id) throws SQLException {
-        return null;
+    public List<Appointment> findAll() throws SQLException {
+        List<Appointment> result = new ArrayList<>();
+        try(PreparedStatement pst = this.conn.prepareStatement(FINDALL)) {
+            try(ResultSet res = pst.executeQuery()) {
+                while (res.next()) {
+                    Appointment appointment = new Appointment();
+                    appointment.setId_appointment(res.getInt("id_appointment"));
+                    appointment.setStartTime(res.getTime("startTime").toLocalTime());
+                    appointment.setEndTime(res.getTime("endTime").toLocalTime());
+                    appointment.setDate(res.getDate("date").toLocalDate());
+                    ClientDAO cdao = new ClientDAO(this.conn);
+                    Client client = cdao.findById(res.getInt("id_client"));
+                    appointment.setClient(client);
+
+                    SpaceDAO adao = new SpaceDAO(this.conn);
+                    Space space = adao.findById(res.getInt("id_space"));
+                    appointment.setSpace(space);
+
+                    result.add(appointment);
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Appointment findById(int id_appointment) throws SQLException {
+        Appointment result = null;
+        try(PreparedStatement pst = this.conn.prepareStatement(FINDBYID)) {
+            pst.setInt(1, id_appointment);
+            try(ResultSet res = pst.executeQuery()) {
+                if(res.next()) {
+                    Appointment appointment = new Appointment();
+                    appointment.setId_appointment(res.getInt("id_appointment"));
+                    appointment.setStartTime(res.getTime("startTime").toLocalTime());
+                    appointment.setEndTime(res.getTime("endTime").toLocalTime());
+                    appointment.setDate(res.getDate("date").toLocalDate());
+
+                    ClientDAO cdao = new ClientDAO(this.conn);
+                    Client client = cdao.findById(res.getInt("id_client"));
+                    appointment.setClient(client);
+
+                    SpaceDAO sdao = new SpaceDAO(this.conn);
+                    Space space = sdao.findById(res.getInt("id_space"));
+                    appointment.setSpace(space);
+                    result = appointment;
+                }
+            }
+        }
+        return result;
     }
 
     @Override
     public Appointment save(Appointment entity) throws SQLException {
-        return null;
+        Appointment result = new Appointment();
+        if(entity != null) {
+            Appointment appointment = findById(entity.getId_appointment());
+
+            ClientDAO cdao = new ClientDAO(this.conn);
+            Client myClient = cdao.findById(entity.getClient().getId_person());
+
+            SpaceDAO sdao = new SpaceDAO(this.conn);
+            Space mySpace = sdao.findById(entity.getSpace().getId_space());
+
+            if(appointment == null) {
+                if(myClient == null & mySpace == null){
+                    cdao.save(entity.getClient());
+                    sdao.save(entity.getSpace());
+                }
+                try(PreparedStatement pst = this.conn.prepareStatement(INSERT)) {
+                    pst.setInt(1, entity.getId_appointment());
+                    pst.setTime(2, Time.valueOf(entity.getStartTime()));
+                    pst.setTime(3, Time.valueOf(entity.getEndTime()));
+                    pst.setDate(4, Date.valueOf(entity.getDate()));
+                    pst.setInt(5, entity.getClient().getId_person());
+                    pst.setInt(6, entity.getSpace().getId_space());
+                    pst.executeUpdate();
+                }
+
+
+            }
+            result = entity;
+        }
+        return result;
     }
 
     @Override
     public Appointment update(Appointment entity) throws SQLException {
-        return null;
+        Appointment result = new Appointment();
+        if(entity != null) {
+            Appointment appointment = findById(entity.getId_appointment());
+
+            ClientDAO cdao = new ClientDAO(this.conn);
+            Client myClient = cdao.findById(entity.getClient().getId_person());
+
+            SpaceDAO sdao = new SpaceDAO(this.conn);
+            Space mySpace = sdao.findById(entity.getSpace().getId_space());
+
+            if(appointment != null) {
+                if(myClient == null & mySpace == null){
+                    cdao.save(entity.getClient());
+                    sdao.save(entity.getSpace());
+                }
+                try(PreparedStatement pst = this.conn.prepareStatement(UPDATE)) {
+                    pst.setTime(1, Time.valueOf(entity.getStartTime()));
+                    pst.setTime(2, Time.valueOf(entity.getEndTime()));
+                    pst.setDate(3, Date.valueOf(entity.getDate()));
+                    pst.setInt(4, entity.getClient().getId_person());
+                    pst.setInt(5, entity.getSpace().getId_space());
+                    pst.executeUpdate();
+                }
+
+
+            }
+            result = entity;
+        }
+        return result;
     }
 
     @Override
     public void delete(Appointment entity) throws SQLException {
-
+        if(entity != null) {
+            try(PreparedStatement pst = this.conn.prepareStatement(DELETE)) {
+                pst.setInt(1, entity.getId_appointment());
+                pst.executeUpdate();
+            }
+        }
     }
 
     @Override
