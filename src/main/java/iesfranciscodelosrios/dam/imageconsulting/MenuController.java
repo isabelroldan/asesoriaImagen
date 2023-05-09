a3d283ace49b3a456ed405da919152873b1c623a
Isabel <isabelroldancordoba@hotmail.com>
2023-05-09 21:54:50 +0200

    estilo menu hamburguesa

--- src/main/java/iesfranciscodelosrios/dam/imageconsulting/MenuController.java
+++ src/main/java/iesfranciscodelosrios/dam/imageconsulting/MenuController.java
@@ -8,25 +8,43 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
 public class MenuController {
 
     @FXML
-    private Label profileLabel;
+    private void handleClientesBtn(ActionEvent event) {
+        // Lógica para manejar la selección de clientes
+        System.out.println("Selected clientes");
+    }
 
     @FXML
-    private void handleProfileClick(MouseEvent event) {
-        // Lógica para manejar la selección del perfil
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Perfil");
-        alert.setHeaderText(null);
-        alert.setContentText("Ha seleccionado la opción 'Perfil'");
-        alert.showAndWait();
+    private void handleProfesionalesBtn(ActionEvent event) {
+        // Lógica para manejar la selección de profesionales
+        System.out.println("Selected profesionales");
+    }
+
+    @FXML
+    private void handleEspaciosBtn(ActionEvent event) {
+        // Lógica para manejar la selección de espacios
+        System.out.println("Selected espacios");
+    }
+
+    @FXML
+    private void handleCitasBtn(ActionEvent event) {
+        // Lógica para manejar la selección de citas
+        System.out.println("Selected citas");
     }
 
     @FXML
     private void handleExitClick(ActionEvent event) {
         // Lógica para manejar la selección de salir
-        Alert alert = new Alert(AlertType.CONFIRMATION);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Salir");
         alert.setHeaderText("¿Está seguro de que desea salir?");
         alert.setContentText("Si selecciona 'Aceptar', la aplicación se cerrará.");
