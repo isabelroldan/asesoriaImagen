2423ddf6f7363c3557507b63f223b38a68209cdd
Isabel <isabelroldancordoba@hotmail.com>
2023-05-19 22:18:35 +0200

    test

--- src/test/java/iesfranciscodelosrios/dam/model/dao/ClientDAOTest.java
+++ src/test/java/iesfranciscodelosrios/dam/model/dao/ClientDAOTest.java
@@ -1,4 +1,119 @@
+package iesfranciscodelosrios.dam.model.dao;
+
+import iesfranciscodelosrios.dam.model.domain.Client;
+import iesfranciscodelosrios.dam.model.domain.ColorTestResult;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ClientDAOTest {
-  
+    static ClientDAO clientDAO;
+    @BeforeAll
+    public static void setUpClass(){
+        clientDAO = new ClientDAO();
+    }
+
+    @Test
+    @DisplayName("Verificación de que la lista de clientes no sea nula y que no esté vacía")
+    void findAll() throws SQLException {
+        List<Client> clients = clientDAO.findAll();
+
+        assertNotNull(clients); // Verifica que la lista de clientes no sea nula
+        assertFalse(clients.isEmpty()); // Verifica que la lista de clientes no esté vacía
+    }
+
+    @Test
+    @DisplayName("Verificación que el cliente no sea nulo y que el id del cliente sea el esperado")
+    void findById() throws SQLException {
+        int clientId = 1; // ID del cliente existente en la base de datos
+
+        Client client = clientDAO.findById(clientId);
+
+        assertNotNull(client); // Verifica que el cliente no sea nulo
+        assertEquals(clientId, client.getId_person()); // Verifica que el ID del cliente sea el esperado
+    }
+
+    @Test
+    @DisplayName("Verificación que el cliente no sea nulo y que el email de este sea el esperado")
+    void findByEmail() throws SQLException {
+        String email = "isabelroldancordoba@hotmail.com"; // Email del cliente existente en la base de datos
+
+        Client client = clientDAO.findByEmail(email);
+
+        assertNotNull(client); // Verifica que el cliente no sea nulo
+        assertEquals(email, client.getEmail()); // Verifica que el email del cliente sea el esperado
+    }
+
+    @Test
+    @DisplayName("Verifica que el id exista en la bbdd")
+    void checkIfIdExists() throws SQLException {
+        int clientId = 1;
+
+        boolean idExists = clientDAO.checkIfIdExists(clientId);
+
+        assertTrue(idExists); // Verifica que el ID exista en la base de datos
+    }
+
+    @Test
+    @DisplayName("Verificación que el email exista en la bbdd")
+    void checkIfEmailExists() throws SQLException {
+        String email = "isabelroldancordoba@hotmail.com";
+
+        boolean emailExists = clientDAO.checkIfEmailExists(email);
+
+        assertTrue(emailExists); // Verifica que el email exista en la base de datos
+    }
+
+    @Test
+    @DisplayName("Verificación que el cliente guardado no sea nulo y que el id del cliente sea el esperado")
+    void save() throws SQLException {
+        Client client = new Client();
+        client.setId_person(75); // Definir un ID que no exista en la base de datos
+        client.setName("ejemplo");
+        client.setSurname("ejemplo..");
+        client.setTelephone("123456789");
+        client.setEmail("ejemploejemplo@example.com");
+        client.setPassword("contraseña");
+        client.setColorTestResult(ColorTestResult.SUMMER);
+
+        Client savedClient = clientDAO.save(client);
+
+        assertNotNull(savedClient); // Verifica que el cliente guardado no sea nulo
+        assertEquals(client.getId_person(), savedClient.getId_person()); // Verifica que el ID del cliente sea el esperado
+    }
+
+    @Test
+    @DisplayName("Verificación que el cliente actualizado no sea nulo y que el nombre del cliente sea el esperado")
+    void update() throws SQLException {
+        Client client = new Client();
+        client.setId_person(75); // Definir un ID existente en la base de datos
+        client.setName("Updated Name");
+        client.setSurname("Updated Surname");
+        client.setTelephone("987654321");
+        client.setEmail("updatedemail@example.com");
+        client.setPassword("newpassword");
+        client.setColorTestResult(ColorTestResult.SPRINT);
+
+        Client updatedClient = clientDAO.update(client);
+
+        assertNotNull(updatedClient); // Verifica que el cliente actualizado no sea nulo
+        assertEquals(client.getName(), updatedClient.getName()); // Verifica que el nombre del cliente sea el esperado
+    }
+
+    @Test
+    @DisplayName("Verificación que el cliente no se encuentre en la bbdd una vez ha sido borrado")
+    void delete() throws SQLException {
+        Client client = new Client();
+        client.setId_person(75); // Definir un ID existente en la base de datos
+
+        clientDAO.delete(client);
+
+        Client deletedClient = clientDAO.findById(client.getId_person());
+        assertNull(deletedClient); // Verifica que el cliente no se encuentre en la base de datos
+    }
 }
--- src/test/java/iesfranciscodelosrios/dam/model/dao/SpaceDAOTest.java
+++ src/test/java/iesfranciscodelosrios/dam/model/dao/SpaceDAOTest.java
@@ -1,4 +1,107 @@
+package iesfranciscodelosrios.dam.model.dao;
+
+import iesfranciscodelosrios.dam.model.domain.Space;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SpaceDAOTest {
-  
+    static SpaceDAO spaceDAO;
+    @BeforeAll
+    public static void setUpClass(){
+        spaceDAO = new SpaceDAO();
+    }
+    @Test
+    @DisplayName("Comprobación para numeros de espacios en la base de datos, comprobación si es cirto que devuelve List<Space> y " +
+            "creamos un List de Space e introducimos todos los datos que tenemos en esa tabla y lo comprobamos por posicion en el ArrayList")
+    void findAll() {
+        try {
+            assertEquals(2, spaceDAO.findAll().size());
+            assertTrue(spaceDAO.findAll() instanceof List<Space>);
+            List<Space> spaceList = new ArrayList<Space>();
+            spaceList.addAll(spaceDAO.findAll());
+            assertEquals("prueba", spaceList.get(1).getName());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Comprobamos que lo que indicamos sea igual al serviceType extraido por id y" +
+            " comprobación de que al buscar un id de la base de datos no sea nulo")
+    void findById(){
+        int id_space = 1;
+        try {
+            assertEquals("pruebaArreglando", spaceDAO.findById(id_space).getServiceType());
+
+            Space result = spaceDAO.findById(id_space);
+
+            assertNotNull(result);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Verifica que el id existente se encuentra en la bbdd y verificación que el id que no existe no este en la bbdd")
+    void checkIfIdExists(){
+        try {
+            int existingId = 1; // ID existente en la base de datos
+            int nonExistingId = 100; // ID que no existe en la base de datos
+
+            boolean existingResult = spaceDAO.checkIfIdExists(existingId);
+            boolean nonExistingResult = spaceDAO.checkIfIdExists(nonExistingId);
+
+            assertTrue(existingResult); // Verifica que el ID existente se encuentre en la base de datos
+            assertFalse(nonExistingResult); // Verifica que el ID que no existe no se encuentre en la base de datos
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    @DisplayName("Verificación para ver que se guarda un nuevo espacio")
+    void save() throws SQLException {
+        Space newSpace = new Space(3, "New Space", "Service B"); // Nuevo espacio a guardar
+
+        Space savedNewSpace = spaceDAO.save(newSpace);
+
+        assertEquals(newSpace, spaceDAO.findById(newSpace.getId_space())); // Verifica que el nuevo espacio se haya guardado correctamente en la base de datos
+    }
+
+    @Test
+    @DisplayName("Verificación que el espacio actualizado no sea nulo, que el espacio actualizado sea igual al modificado y " +
+            "que el espacio de la bbdd sea igual al modificado")
+    void update() throws SQLException {
+        Space existingSpace = new Space(3, "New Space", "Service B"); // Espacio a modificar
+
+        // Modificamos el espacio existente
+        existingSpace.setName("Updated Space");
+        existingSpace.setServiceType("Service A");
+
+        Space updatedSpace = spaceDAO.update(existingSpace);
+
+        assertNotNull(updatedSpace); // Verifica que el espacio actualizado no sea nulo
+        assertEquals(existingSpace, updatedSpace); // Verifica que el espacio actualizado sea igual al espacio modificado
+        Space fetchedSpace = spaceDAO.findById(existingSpace.getId_space());
+        assertEquals(existingSpace, fetchedSpace); // Verifica que el espacio en la base de datos sea igual al espacio modificado
+    }
+
+    @Test
+    @DisplayName("Verificación de que el espacio haya sido eliminado en la bbdd")
+    void delete() throws SQLException{
+        Space existingSpace = new Space(3, "Updated Space", "Service A"); // Espacio a borrar
+
+        spaceDAO.delete(existingSpace);
+
+        Space deletedSpace = spaceDAO.findById(existingSpace.getId_space());
+        assertNull(deletedSpace); // Verifica que el espacio haya sido eliminado de la base de datos
+    }
 }
